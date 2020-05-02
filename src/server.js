7c5f0219703cfd4af5d131cefc064fb9effd264e
dny8888 <dny8888@gmail.com>
2020-05-02 00:18:58 -0300

    CRUD users and error handling

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 
+app.use(express.json())
 app.use(routes);
 
-app.get('/users', (req, res) => knex('users').then((results) => res.json(results)))
+//cath all
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+})
 
 app.listen(3333, () => console.log('Server is running'));
