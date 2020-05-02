3c1f0cabb8e8302055e9d6d5e25ad953e9bf6cbb
dny8888 <dny8888@gmail.com>
2020-05-02 13:23:51 -0300

    started CRUD projects

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.json())
 app.use(routes);
 
+//notfound
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
 //cath all
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
